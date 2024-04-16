6ee1cb756215c5445f3f6af1c02d2ef78dc000e2
Herick2D <contato.herickmoreira@gmail.com>
2024-04-16 20:25:58 -0300

    feat: db connection

--- src/main/java/com/herick/controller/GestaoCodecraftBean.java
+++ src/main/java/com/herick/controller/GestaoCodecraftBean.java
@@ -1,11 +1,14 @@
 package com.herick.controller;
 
 import java.io.Serializable;
+import java.util.logging.Logger;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
 
 import com.herick.model.Aluno;
 import com.herick.repository.Alunos;
@@ -20,10 +23,14 @@ public class GestaoCodecraftBean implements Serializable {
 	@Inject
 	private Alunos alunos;
 	
-	private Aluno alunoEscolhido;
+	private Aluno alunoEscolhido = new Aluno();
+	
+	public static final Logger LOGGER = Logger.getLogger(GestaoCodecraftBean.class.getName());
 	
 	public Aluno alunosById() {
-		return  alunos.alunoPorId(1L);
+		EntityManager em = Persistence.createEntityManagerFactory("codecraftPU").createEntityManager();
+		alunoEscolhido = em.find(Aluno.class, 1L);
+		return alunoEscolhido;
 	}
 	
 	
@@ -43,6 +50,10 @@ public class GestaoCodecraftBean implements Serializable {
 
 	public void setAlunoEscolhido(Aluno alunoEscolhido) {
 		this.alunoEscolhido = alunoEscolhido;
+	}
+	
+	public void console() {
+		LOGGER.info("AQUI, EU TO AQUIIIIIIII ############################ Matricula: " + alunoEscolhido.getMatricula());
 	}
 	
 	
--- src/main/java/com/herick/repository/Alunos.java
+++ src/main/java/com/herick/repository/Alunos.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
 
 import com.herick.model.Aluno;
 
@@ -11,20 +12,18 @@ public class Alunos implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	@Inject
-	private EntityManager manager;
-
 	public Alunos(EntityManager manager) {
-		this.manager = manager;
+//		this.em = manager;
 	}
 
 	public Aluno alunoPorId(Long id) {
-		return manager.find(Aluno.class, id);
+		EntityManager em = Persistence.createEntityManagerFactory("codecraftPU").createEntityManager();
+		return em.find(Aluno.class, id);
 	}
 	
 	public void removerAluno(Aluno aluno) {
 		aluno = alunoPorId(aluno.getId());
-		manager.remove(aluno);
+//		em.remove(aluno);
 	}
 	
 }
