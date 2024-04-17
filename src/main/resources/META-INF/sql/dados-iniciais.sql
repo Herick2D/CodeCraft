98298a38bb0655083df6c93a7a759ebe857980f2
MARÇAL MARCOS M <42890965+MarcosMarcal@users.noreply.github.com>
2024-04-16 22:54:27 -0300

    fix db scripts

--- src/main/resources/META-INF/sql/dados-iniciais.sql
+++ src/main/resources/META-INF/sql/dados-iniciais.sql
@@ -1,11 +1,12 @@
 INSERT INTO codecraft.alunos (nome_aluno, matricula_aluno ) VALUES ('Herick Moreira', 0001);
 INSERT INTO codecraft.alunos (nome_aluno, matricula_aluno ) VALUES ('Karina Maciel', 0002);
 
+INSERT INTO codecraft.professores (nome_professor, matricula_professor) VALUES ('Vinicius do Java', 12345);
+INSERT INTO codecraft.professores (nome_professor, matricula_professor) VALUES ('Monique do Kotlin', 54321);
+
 INSERT INTO codecraft.cursos (nome_curso, professor_id) VALUES ('Curso de Java', 1);
-INSERT INTO cursos (nome_curso, professor_id) VALUES ('Curso de Kotlin', 2);
+INSERT INTO codecraft.cursos (nome_curso, professor_id) VALUES ('Curso de Kotlin', 2);
 
-INSERT INTO cursos_alunos (curso_id, aluno_id) VALUES (1, 1);
-INSERT INTO cursos_alunos (curso_id, aluno_id) VALUES (2, 2);
+INSERT INTO codecraft.curso_aluno (curso_id, aluno_id) VALUES (1, 1);
+INSERT INTO codecraft.curso_aluno (curso_id, aluno_id) VALUES (2, 2);
 
-INSERT INTO professores (nome_professor, matricula_professor) VALUES ('Vinicius do Java', 12345);
-INSERT INTO professores (nome_professor, matricula_professor) VALUES ('Monique do Kotlin', 54321);
